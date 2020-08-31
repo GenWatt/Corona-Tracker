89348e355aedba6187b8ac181ed8b70cb37bd75c
GenWatt <adriankosoglos@gmail.com>
2020-08-31 16:38:54 +0200

    Solve line chart issue

--- src/js/components/charts.ts
+++ src/js/components/charts.ts
@@ -1,5 +1,7 @@
 import Chart from "chart.js";
 import { StatisticInterface, ApiDailyInterface } from "../apiInterface";
+import { DOMElements } from "../DOMElements.ts";
+import { GLOBAL } from "../config.ts";
 
 let createdChart;
 
@@ -41,31 +43,42 @@ export function lineChart(data: ApiDailyInterface[]) {
   const ctx = c.getContext("2d");
 
   if (createdChart) createdChart.destroy();
+  const confirmedData: number[] = [];
+  const deathsData: number[] = [];
+  const raportData: Date[] = [];
+
+  data.map(({ confirmed }, index) => {
+    if (index % 5 === 0 || index === data.length) confirmedData.push(confirmed.total);
+  });
+
+  data.map(({ deaths }, index) => {
+    if (index % 5 === 0 || index === data.length) deathsData.push(deaths.total);
+  });
+
+  data.map((details, index) => {
+    if (index % 5 === 0 || index === data.length) raportData.push(details.reportDate);
+  });
+
+  DOMElements.searchHeader.innerText = GLOBAL;
 
   const chart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: data.map((details, index) => {
-        if (index % 5 === 0 || index === data.length) return details.reportDate;
-      }),
+      labels: raportData,
       datasets: [
         {
           label: "Deaths",
           backgroundColor: "red",
           borderColor: "rgb(255, 99, 132)",
           fill: true,
-          data: data.map(({ deaths }, index) => {
-            if (index % 5 === 0 || index === data.length) return deaths.total;
-          }),
+          data: deathsData,
         },
         {
           label: "Confirmed",
           backgroundColor: "yellow",
           fill: true,
           borderColor: "rgb(255, 99, 132)",
-          data: data.map(({ confirmed }, index) => {
-            if (index % 5 === 0 || index === data.length) return confirmed.total;
-          }),
+          data: confirmedData,
         },
       ],
     },
